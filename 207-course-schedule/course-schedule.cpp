624d40989f786527a0ca976b137ec5b9ab139aa5
Sparsh468 <sparsh4681930@gmail.com>
2026-07-31 20:28:08 +0530

    Time: 57 ms (5.04%) | Memory: 17.8 MB (97.04%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isCycleDFS(int src,vector<bool>&vis,vector<bool>&recpath,vector<vector<int>>& edges){
+        vis[src]=true;
+        recpath[src]=true;
+        for(int i=0;i<edges.size();i++){
+            int v=edges[i][0];
+            int u=edges[i][1];
+            if(u==src){
+                if(!vis[v]){
+                    if(isCycleDFS(v,vis,recpath,edges)){
+                        return true;
+                    }
+                }else if(recpath[v]){
+                    return true;
+                }
+            }
+        }
+        recpath[src]=false;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        vector<bool> vis(n,false);
+        vector<bool> recpath(n,false);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                if(isCycleDFS(i,vis,recpath,edges)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
