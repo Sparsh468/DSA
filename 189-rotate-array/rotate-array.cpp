c7671eb93a61d8178dffab42feaf6c88a645a59a
Sparsh <sparsh4681930@gmail.com>
2026-08-22 20:48:47 +0530

    Time: 1 ms (22.66%) | Memory: 263.3 MB (14.26%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,18 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        vector<int> temp(k);
-        for(int i=0;i<k;i++){
-            temp[i]=nums[n-k+i];
-        }
-        for(int i=n-k-1;i>=0;i--){
-            nums[i+k]=nums[i];
-        }
-        for(int i=0;i<k;i++){
-            nums[i]=temp[i];
-        }
-        for(int i=0;i<n;i++){
-            cout<<nums[i];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
