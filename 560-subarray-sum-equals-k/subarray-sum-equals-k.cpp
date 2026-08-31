4090cdac262fae92a3881e3b6331c8109d7d419b
Sparsh <sparsh4681930@gmail.com>
2026-08-31 23:30:57 +0530

    Time: 47 ms (47.94%) | Memory: 51.1 MB (13.96%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        int presum=0,cnt=0;
+        for(int i=0;i<arr.size();i++){
+            presum+=arr[i];
+            int remove=presum-k;
+            cnt+=mpp[remove];
+            mpp[presum]+=1;
+        }
+        return cnt;
+    }
+};
