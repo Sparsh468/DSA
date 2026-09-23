8023d2b47642c4c4e70d7efb08ea632a77e88fb3
Sparsh <sparsh4681930@gmail.com>
2026-09-23 22:29:50 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (95.93%) - LeetSync

--- 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.cpp
+++ 4312-limit-occurrences-in-sorted-array/limit-occurrences-in-sorted-array.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> limitOccurrences(vector<int>& nums, int k) {
-        vector<int> ans;
+        int j=0;
         for(int i=0;i<nums.size();i++){
-            if(i<k||nums[i]!=nums[i-k]){
-                ans.push_back(nums[i]);
+            if(j<k||nums[i]!=nums[j-k]){
+                nums[j]=nums[i];
+                j++;
             }
         }
-        return ans;
+        nums.resize(j);
+        return nums;
     }
 };
