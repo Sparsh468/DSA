dd92ee261343f3a978bb57030a8885da045d1731
Sparsh <sparsh4681930@gmail.com>
2026-09-23 22:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.16%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    int lowerbound(vector<int> &nums,int n,int x){
+    int firstoccurrence(vector<int> &nums,int n,int x){
         int low=0,high=n-1;
-        int ans=n;
+        int first=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>=x){
-                ans=mid;
+            if(nums[mid]==x){
+                first=mid;
                 high=mid-1;
             }
-            else{
+            else if(nums[mid]<x){
                 low=mid+1;
             }
+            else{
+                high=mid-1;
+            }
         }
-        return ans;
+        return first;
     }
-    int upperbound(vector<int> &nums,int n,int x){
+    int lastoccurrence(vector<int> &nums,int n,int x){
         int low=0,high=n-1;
-        int ans=n;
+        int last=-1;
         while(low<=high){
             int mid=(low+high)/2;
-            if(nums[mid]>x){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
+            if(nums[mid]==x){
+                last=mid;
                 low=mid+1;
             }
+            else if(nums[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        return ans;
+        return last;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
-        int lb=lowerbound(nums,n,target);
-        if(lb==n||nums[lb]!=target) return {-1,-1};
-        return{lb, upperbound(nums,n,target)-1};
+        int first=firstoccurrence(nums,n,target);
+        if(first==-1) return {-1,-1};
+        int last=lastoccurrence(nums,n,target);
+        return {first,last};
     }
 };
